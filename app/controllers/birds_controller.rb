e9fd01ba41c622878c2ae5a0f4441fa92d7469c8
PapitoSpence <PapitoSpence>
2023-03-19 14:15:20 +0300

    commit tree 25d948149456efa489ebd35b400b1f5207789344

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -21,5 +21,13 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
+
+end
 
 end
